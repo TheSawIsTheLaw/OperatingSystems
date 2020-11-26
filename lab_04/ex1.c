9e96010a3a234d805442d3acbcaced1cba93619e
theSawIsTheLaw <dimakrok@bk.ru>
2020-11-26 16:33:37 +0300

    ADD: gitignore and first ex

--- lab_04/ex1.c
+++ lab_04/ex1.c
@@ -1,1 +1,36 @@
- 
+ #include <sys/types.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+
+#define CANT_FORK_ERROR 1
+#define SUCCESS 0
+
+ int main(void)
+ {
+    printf("From Parent. Parent identifiers: parentProcID is %d, groupID is %d\n", getpid(), getpgrp());
+    pid_t childpid;
+    int children[2];
+     
+    for (int i = 0; i < 2; i++)
+    {
+        if ((childpid = fork()) == -1)
+        {
+            perror("Can't fork");
+            return CANT_FORK_ERROR;
+        }
+        else if (childpid == 0)
+        { 
+            sleep(1);
+            printf("From child. Child identifiers: childProcID is %d, groupID is %d, parentID is %d\n", getpid(), getpgrp(), getppid());
+            return SUCCESS;
+        }
+        else
+            children[i] = childpid;
+        
+    }
+
+    printf("Children IDs from parent proccess: %d and %d\nEnd of parent existence\n\n", children[0], children[1]);
+
+    return SUCCESS;   
+ }
