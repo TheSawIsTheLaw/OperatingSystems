986e1c7af7418454f4dab9fcb1db27d8c910803f
TrveHazzk3r <dimakrok@bk.ru>
2021-03-25 12:19:35 +0300

    PRP: preparings for show

--- sem_02/lab_03/fTask/md.c
+++ sem_02/lab_03/fTask/md.c
@@ -11,15 +11,15 @@ static int __init md_init(void)
 { 
    printk(KERN_INFO "Module init:\n");
 
-   struct task_struct *task = &init_task; // Инициализация списка декрипторов процессов
+   struct task_struct *task = &init_task;
    do
    {
        printk(KERN_INFO "~~~~~~ Proccess: %s (comm) - %d (ID), Parent: %s (comm) - %d (ID)\n", 
             task->comm, task->pid, task->parent->comm, task->parent->pid);
-   } while ((task = next_task(task)) != &init_task); // Прохождения списка
+   } while ((task = next_task(task)) != &init_task);
 
    printk(KERN_INFO "~~~~~~ Proccess: %s (comm) - %d (ID), Parent: %s (comm) - %d (ID)\n",
-		current->comm, current->pid, current->parent->comm, current->parent->pid); // получение информации о текущем процессе
+		current->comm, current->pid, current->parent->comm, current->parent->pid);
 
    return 0; 
 }
