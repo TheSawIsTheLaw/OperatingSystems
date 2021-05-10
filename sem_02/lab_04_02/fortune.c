828917d81b01f7e651bbd6b2590f0e27ec9c8971
TrveHazzk3r <dimakrok@bk.ru>
2021-05-10 15:47:00 +0300

    ADD: 4.2 lab

--- sem_02/lab_04_02/fortune.c
+++ sem_02/lab_04_02/fortune.c
@@ -5,7 +5,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yakuba D.V.");
 
-#define MAX_COOKIE_LENGTH PAGE_SIZE
+#define MAX_LEN PAGE_SIZE
 
 #define FORTUNE_DIRNAME "fortDir"
 #define FORTUNE_FILENAME "fortFile"
@@ -16,62 +16,66 @@ static struct proc_dir_entry *fortDir = NULL;
 static struct proc_dir_entry *fortFile = NULL;
 static struct proc_dir_entry *fortSymlink = NULL;
 
-static char *cookiePot; // Хранилище наших фортунок
-static int cookieIndex; // Индекс для добавления печеньки
-static int nextFortune; // Индекс для чтения печеньки
+static char *fortunesArr; // Введённые фортунки
+static int writeIndex; // Индекс для добавления
+static int readIndex; // Индекс для чтения
 
-static char tmp[MAX_COOKIE_LENGTH];
+static char copy[MAX_LEN];
 
-ssize_t fortune_read(struct file *filep, char __user *buf, size_t count, loff_t *offp)
+ssize_t fortRead(struct file *filep, char __user *buf, size_t count, loff_t *offp)
 {
-    if (*offp > 0 || !cookieIndex)
+    printk(KERN_INFO "--***-- FORT: called read\n");
+    
+    if (*offp > 0 || !writeIndex)
         return 0;
 
-    if (nextFortune >= cookieIndex)
-        nextFortune = 0;
+    if (readIndex >= writeIndex)
+        readIndex = 0;
 
-    int len = snprintf(tmp, MAX_COOKIE_LENGTH, "%s\n", &cookiePot[nextFortune]);
-    if (copy_to_user(buf, tmp, len))
+    int len = snprintf(copy, MAX_LEN, "%s\n", &fortunesArr[readIndex]);
+    if (copy_to_user(buf, copy, len))
     {
         printk(KERN_ERR "--***-- FORT: copy_to_user error\n");
         return -EFAULT;
     }
 
-    nextFortune += len;
+    readIndex += len;
     *offp += len;
 
     return len;
 }
 
-ssize_t fortune_write(struct file *file, const char __user *buf, size_t len, loff_t *offp)
+ssize_t fortWrite(struct file *file, const char __user *buf, size_t len, loff_t *offp)
 {
-    if (len > MAX_COOKIE_LENGTH - cookieIndex + 1)
+    printk(KERN_INFO "--***-- FORT: called write\n");
+
+    if (len > MAX_LEN - writeIndex + 1)
     {
-        printk(KERN_ERR "--***-- FORT: cookiePot overflow error\n");
+        printk(KERN_ERR "--***-- FORT: fortunesArr overflow error\n");
         return -ENOSPC; // No enough space
     }
 
-    if (copy_from_user(&cookiePot[cookieIndex], buf, len))
+    if (copy_from_user(&fortunesArr[writeIndex], buf, len))
     {
         printk(KERN_ERR "--***-- FORT: copy_to_user error\n");
         return -EFAULT; // Bad address
     }
 
-    cookieIndex += len;
-    cookiePot[cookieIndex - 1] = '\0';
+    writeIndex += len;
+    fortunesArr[writeIndex - 1] = '\0';
 
     return len;
 }
 
 // cat /proc/fortune -> result
-int fortune_open(struct inode *spInode, struct file *spFile)
+int fortOpen(struct inode *spInode, struct file *spFile)
 {
     printk(KERN_INFO "--***-- FORT: called open\n");
 
     return 0;
 }
 
-int fortune_release(struct inode *spInode, struct file *spFile)
+int fortRelease(struct inode *spInode, struct file *spFile)
 {
     printk(KERN_INFO "--***-- FORT: called release\n");
 
@@ -80,10 +84,10 @@ int fortune_release(struct inode *spInode, struct file *spFile)
 
 static struct proc_ops fops =
 {
-    proc_read : fortune_read, 
-    proc_write : fortune_write, 
-    proc_open : fortune_open, 
-    proc_release : fortune_release
+    proc_read : fortRead, 
+    proc_write : fortWrite, 
+    proc_open : fortOpen, 
+    proc_release : fortRelease
 };
 
 static void cleanup(void)
@@ -94,8 +98,8 @@ static void cleanup(void)
         remove_proc_entry(FORTUNE_FILENAME, fortDir);
     if (fortDir)
         remove_proc_entry(FORTUNE_DIRNAME, NULL);
-    if (cookiePot)
-        vfree(cookiePot);
+    if (fortunesArr)
+        vfree(fortunesArr);
 }
 
 static int noMemoryErrorExit(const char *s)
@@ -105,11 +109,11 @@ static int noMemoryErrorExit(const char *s)
     return -ENOMEM; // No memory
 }
 
-static int __init fortune_init(void)
+static int __init fortInit(void)
 {
-    if (!(cookiePot = vmalloc(MAX_COOKIE_LENGTH)))
+    if (!(fortunesArr = vmalloc(MAX_LEN)))
         return noMemoryErrorExit("vmalloc");
-    memset(cookiePot, 0, MAX_COOKIE_LENGTH);
+    memset(fortunesArr, 0, MAX_LEN);
 
     if (!(fortDir = proc_mkdir(FORTUNE_DIRNAME, NULL)))
         return noMemoryErrorExit("proc_mkdir");
@@ -118,18 +122,18 @@ static int __init fortune_init(void)
     if (!(fortSymlink = proc_symlink(FORTUNE_SYMLINK, NULL, FORTUNE_FILEPATH)))
         return noMemoryErrorExit("proc_symlink");
 
-    cookieIndex = 0;
-    nextFortune = 0;
+    writeIndex = 0;
+    readIndex = 0;
 
     printk(KERN_INFO "--***-- FORT: module loaded\n");
     return 0;
 }
 
-static void __exit fortune_exit(void)
+static void __exit fortExit(void)
 {
     cleanup();
     printk(KERN_INFO "--***-- FORT: module unloaded\n");
 }
 
-module_init(fortune_init) 
-module_exit(fortune_exit)
+module_init(fortInit) 
+module_exit(fortExit)
---
+++ sem_02/lab_04_02/fortuneseq.c
@@ -0,0 +1,117 @@
+#include <linux/module.h> 
+#include <linux/kernel.h> 
+#include <linux/init.h>  
+#include <linux/vmalloc.h>
+#include <linux/proc_fs.h> 
+#include <linux/uaccess.h>
+#include <linux/seq_file.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yakuba Dmitry");
+
+#define MAX_LEN PAGE_SIZE
+
+#define DIRNAME "fortSeqDir"
+#define FILENAME "fortSeqFile"
+#define SYMLINK "fortSeq"
+#define FORTUNE_FILEPATH DIRNAME "/" FILENAME
+
+static struct proc_dir_entry *fortDir = NULL;
+static struct proc_dir_entry *fortFile = NULL;
+static struct proc_dir_entry *fortSymlink = NULL;
+
+static char *fortunesArr;
+static int writeIndex;
+static int readIndex;
+
+static char copy[MAX_LEN];
+
+int fortShow(struct seq_file* filep, void* v) {
+    printk(KERN_INFO "SEQ--***-- FORT: called show\n");
+    int len;
+    if (!writeIndex)
+        return 0;
+    len = snprintf(copy, MAX_LEN, "%s", &fortunesArr[readIndex]);
+    seq_printf(filep, "%s", copy);
+    readIndex += len;
+    return 0;
+}
+
+ssize_t fortWrite(struct file *filp, const char __user *buf, size_t len, loff_t *offp) {
+    printk(KERN_INFO "SEQ--***-- FORT: called цкшеу\n");
+    if (len > MAX_LEN - writeIndex + 1) {
+        printk(KERN_ERR "SEQ--***-- FORT: fortunesArr overflow error\n");
+        return -ENOSPC; // No enough space
+    }
+    if (copy_from_user(&fortunesArr[writeIndex], buf, len)) {
+        printk(KERN_ERR "SEQ--***-- FORT: copy_to_user error\n");
+        return -EFAULT; // Bad address
+    }
+    
+    writeIndex += len - 1;
+    fortunesArr[writeIndex] = '\0';
+    return len;
+}
+
+int fortOpen(struct inode *inode, struct file *file) {
+    printk(KERN_INFO "SEQ--***-- FORT: called open\n");
+    return single_open(file, fortShow, NULL);
+}
+
+int fortRelease(struct inode *inode, struct file *file) {
+    printk(KERN_INFO "SEQ--***-- FORT: called release\n");
+    return single_release(inode, file);
+}
+
+static struct proc_ops fops = {
+    proc_read: seq_read, 
+    proc_write: fortWrite, 
+    proc_open: fortOpen, 
+    proc_release: fortRelease
+};
+
+static void cleanup(void) {
+    if (fortSymlink)
+        remove_proc_entry(SYMLINK, NULL);
+    if (fortFile)
+        remove_proc_entry(FILENAME, fortDir);
+    if (fortDir)
+        remove_proc_entry(DIRNAME, NULL);
+    if (fortunesArr)
+        vfree(fortunesArr);
+}
+
+static int noMemoryErrorExit(const char *s)
+{
+    cleanup();
+    printk(KERN_ERR "SEQ--***-- FORT: %s error\n", s);
+    return -ENOMEM; // No memory
+}
+
+static int __init fortInit(void)
+{
+    if (!(fortunesArr = vmalloc(MAX_LEN)))
+        return noMemoryErrorExit("vmalloc");
+    memset(fortunesArr, 0, MAX_LEN);
+
+    if (!(fortDir = proc_mkdir(DIRNAME, NULL)))
+        return noMemoryErrorExit("proc_mkdir");
+    if (!(fortFile = proc_create(FILENAME, 0666, fortDir, &fops)))
+        return noMemoryErrorExit("proc_create");
+    if (!(fortSymlink = proc_symlink(SYMLINK, NULL, FORTUNE_FILEPATH)))
+        return noMemoryErrorExit("proc_symlink");
+
+    writeIndex = 0;
+    readIndex = 0;
+
+    printk(KERN_INFO "SEQ--***-- FORT: module loaded\n");
+    return 0;
+}
+
+static void __exit fortExit(void) {
+    cleanup();
+    printk(KERN_INFO "SEQ--***-- FORT: called read\n");
+}
+
+module_init(fortInit) 
+module_exit(fortExit)
