36e858a03d488bc54be35f9ba1a294510145d26e
TrveHazzk3r <dimakrok@bk.ru>
2021-06-10 01:17:05 +0300

    FIX: gm. Seems like not a dynamic but...

--- sem_02/lab_09/workqueue/yakubaWorkqueue.c
+++ sem_02/lab_09/workqueue/yakubaWorkqueue.c
@@ -1,61 +1,104 @@
+#include <asm/io.h>
+#include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/interrupt.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 #include <linux/workqueue.h>
-#include <asm/io.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yakuba D.");
 
 #define IRQ 1
-static int dev_id;
+static int devID;
 
-static struct workqueue_struct *my_wq;
+struct workqueue_struct *workQueue;
 
-void my_workqueue_function(struct work_struct *work)
+void queueFunctionF(struct work_struct *work)
 {
-	const int scancode = inb(0x60);
-	if (scancode < 103) {
-		printk(KERN_INFO "my_workqueue: KEYBOARD INTERRUPT: scancode: %d\n", scancode);
-	}
+    printk(KERN_INFO "<<<yakubaQueue: Key was clicked (1 worker)\n");
 }
 
-DECLARE_WORK(workname, my_workqueue_function);
-
-irqreturn_t my_irq_handler(int irq, void *dev)
+void queueFunctionS(struct work_struct *work)
 {
-	if (irq == IRQ) {
-		queue_work(my_wq, &workname);
-		return IRQ_HANDLED;
-	}
-	return IRQ_NONE;
+    printk(KERN_INFO "<<<yakubaQueue: Key was clicked (2 worker)\n");
 }
 
-static int __init my_workqueue_init(void)
+struct work_struct fWork;
+struct work_struct sWork;
+
+irqreturn_t handler(int irq, void *dev)
 {
-	int ret = request_irq(IRQ, my_irq_handler, IRQF_SHARED, "my_irq_handler", &dev_id);
-	if (ret) {
-		printk(KERN_ERR "my_tasklet: my_irq_handler wasn't registered\n");
-		return ret;
-	}
-
-	if (!(my_wq = create_workqueue("my_queue"))) {
-		free_irq(IRQ, &dev_id);
-		printk(KERN_INFO "my_workqueue: workqueue wasn't created");
-		return -ENOMEM;
-	}
-
-	printk(KERN_INFO "my_workqueue: module loaded\n");
-	return 0;
+    printk(KERN_INFO "<<<yakubaQueue: move work to queue...\n");
+    if (irq == IRQ)
+    {
+        queue_work(workQueue, &fWork);
+        queue_work(workQueue, &sWork);
+        return IRQ_HANDLED;
+    }
+    return IRQ_NONE;
 }
 
-static void __exit my_workqueue_exit(void)
+static struct proc_dir_entry *file = NULL;
+
+int procShow(struct seq_file *filep, void *v)
 {
-	flush_workqueue(my_wq);
-	destroy_workqueue(my_wq);
-	free_irq(IRQ, &dev_id);
-	printk(KERN_INFO "my_workqueue: module unloaded\n");
+    printk(KERN_INFO "<<<yakubaQueue: called show\n");
+    seq_printf(filep, "Data of a 1 work: %d", fWork.data);
+    seq_printf(filep, "Data of a 2 work: %d", sWork.data);
+    return 0;
 }
 
-module_init(my_workqueue_init)
-module_exit(my_workqueue_exit)
+int procOpen(struct inode *inode, struct file *fileInner)
+{
+    printk(KERN_INFO "<<<yakubaQueue: called open\n");
+    return single_open(fileInner, procShow, NULL);
+}
+
+int procRelease(struct inode *inode, struct file *fileInner)
+{
+    printk(KERN_INFO "<<<yakubaQueue: called release\n");
+    return single_release(inode, fileInner);
+}
+
+static struct proc_ops fops = {proc_read : seq_read, proc_open : procOpen, proc_release : procRelease};
+
+static int __init workQueueInit(void)
+{
+    int ret = request_irq(IRQ, handler, IRQF_SHARED, "handler", &devID);
+    if (ret)
+    {
+        printk(KERN_ERR "<<<yakubaQueue: handler wasn't registered\n");
+        return ret;
+    }
+
+    if (!(workQueue = create_workqueue("yakubaQueue")))
+    {
+        free_irq(IRQ, &devID);
+        printk(KERN_INFO "<<<yakubaQueue: workqueue wasn't created");
+        return -ENOMEM;
+    }
+
+    INIT_WORK(&fWork, queueFunctionF);
+    INIT_WORK(&sWork, queueFunctionS);
+
+    if (!proc_create("workqueue", 0666, file, &fops))
+    {
+        printk(KERN_INFO "<<<yakubaQueue: cannot proc_create!\n");
+        return -1;
+    }
+
+    printk(KERN_INFO "<<<yakubaQueue: module loaded\n");
+    return 0;
+}
+
+static void __exit workQueueExit(void)
+{
+    flush_workqueue(workQueue);
+    destroy_workqueue(workQueue);
+    free_irq(IRQ, &devID);
+    remove_proc_entry("workqueue", NULL);
+    printk(KERN_INFO "<<<yakubaQueue: module unloaded\n");
+}
+
+module_init(workQueueInit) module_exit(workQueueExit)
